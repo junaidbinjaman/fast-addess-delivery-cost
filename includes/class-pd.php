60fb13a0100b9b9443a87fce447c1b46acf9323b
junaidbinjaman <me@junaidbinjaman.com>
2024-06-12 11:25:41 +0600

    README.md file, licence.txt files are written. The default php files those comes with wppb are now phpcs comfortable

--- includes/class-pd.php
+++ includes/class-pd.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The file that defines the core plugin class
  *
@@ -78,7 +77,6 @@ class Pd {
 		$this->set_locale();
 		$this->define_admin_hooks();
 		$this->define_public_hooks();
-
 	}
 
 	/**
@@ -103,27 +101,26 @@ class Pd {
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pd-loader.php';
+		require_once plugin_dir_path( __DIR__ ) . 'includes/class-pd-loader.php';
 
 		/**
 		 * The class responsible for defining internationalization functionality
 		 * of the plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-pd-i18n.php';
+		require_once plugin_dir_path( __DIR__ ) . 'includes/class-pd-i18n.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-pd-admin.php';
+		require_once plugin_dir_path( __DIR__ ) . 'admin/class-pd-admin.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the public-facing
 		 * side of the site.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-pd-public.php';
+		require_once plugin_dir_path( __DIR__ ) . 'public/class-pd-public.php';
 
 		$this->loader = new Pd_Loader();
-
 	}
 
 	/**
@@ -140,7 +137,6 @@ class Pd {
 		$plugin_i18n = new Pd_i18n();
 
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
-
 	}
 
 	/**
@@ -156,7 +152,6 @@ class Pd {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
 	}
 
 	/**
@@ -172,7 +167,6 @@ class Pd {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
 	}
 
 	/**
@@ -214,5 +208,4 @@ class Pd {
 	public function get_version() {
 		return $this->version;
 	}
-
 }
